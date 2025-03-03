fda6e2b4257db82dcf764bf0d5c06ad7dbd7738c
JIHAN KUSUMAWARDHANI <jihankusumawwardhani@gmail.com>
2025-03-03 20:55:33 +0700

    dashboard.py

--- submission/dashboard/dashboard.py
+++ submission/dashboard/dashboard.py
@@ -109,11 +109,8 @@ with st.sidebar:
             <div>🆔<span>:</span> jihankusumawardhani</div>
         </div>
     """, unsafe_allow_html=True)
-    start_date, end_date = st.date_input(
-    "Pilih Rentang Waktu",
-    value=(min_date, max_date),  # Gunakan tuple, bukan list
-    min_value=min_date,
-    max_value=max_date
+     min_date, max_date = all_df['dateday'].min(), all_df['dateday'].max()
+    start_date, end_date = st.date_input("Pilih Rentang Waktu", min_value=min_date, max_value=max_date, value=[min_date, max_date])
 )
     
     st.markdown("""
